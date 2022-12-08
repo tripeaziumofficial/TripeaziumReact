9f33833f64b2c800a5f9f7e1669867802dfc4a31
Retro1055 <42443575+Retro1055@users.noreply.github.com>
2022-12-08 16:30:38 +0530

    Getting location

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [latitude,setLatitude]=useState('')
+  const [longitude,setLongitude]=useState('')
+  React.useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
